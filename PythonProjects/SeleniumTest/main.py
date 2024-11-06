09e237147e6aae1eac934f10ba1e53f09f27480d
Ilmas Sibgatullyn <irbis116rus@icloud.com>
2024-11-06 13:17:46 +0300

    Update main.py

--- PythonProjects/SeleniumTest/main.py
+++ PythonProjects/SeleniumTest/main.py
@@ -4,16 +4,16 @@ URL = "https://api.pokemonbattle.ru/v2"
 TOKEN = "343fb6f9d0b2d7732cd588633f3b1eb1"
 HEADR = {"Content-Type":"application/json", "trainer_token":TOKEN}
 body_create_pokemons = {
-    "name": "Jeck",
-    "photo_id": 333
+    "name": "Jon",
+    "photo_id": 321
 }
 body_name_change_pokemons = {
-    "pokemon_id": "119790",
-    "name": "Bob",
-    "photo_id": 2
+    "pokemon_id": "125119",
+    "name": "Dog",
+    "photo_id": 20
 }
 body_catch_in_a_pokeball = {
-    "pokemon_id": "121117"
+    "pokemon_id": "125119"
 }
 
 create_pokemons = requests.post(url = f"{URL}/pokemons", headers = HEADR, json = body_create_pokemons)
