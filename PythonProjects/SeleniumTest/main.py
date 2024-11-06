73e288a6da5ac0391416bde8a19ea62fd512fb1d
Ilmas Sibgatullyn <irbis116rus@icloud.com>
2024-11-06 13:05:28 +0300

    Update main.py

--- PythonProjects/SeleniumTest/main.py
+++ PythonProjects/SeleniumTest/main.py
@@ -16,11 +16,11 @@ body_catch_in_a_pokeball = {
     "pokemon_id": "121117"
 }
 
-"""create_pokemons = requests.post(url = f"{URL}/pokemons", headers = HEADR, json = body_create_pokemons)
-print(create_pokemons.text)"""
+create_pokemons = requests.post(url = f"{URL}/pokemons", headers = HEADR, json = body_create_pokemons)
+print(create_pokemons.text)
 
-"""name_change_pokemons = requests.put(url = f"{URL}/pokemons", headers = HEADR, json = body_name_change_pokemons)
-print(name_change_pokemons.text)"""
+name_change_pokemons = requests.put(url = f"{URL}/pokemons", headers = HEADR, json = body_name_change_pokemons)
+print(name_change_pokemons.text)
 
-"""catch_in_a_pokeball = requests.post(url = f"{URL}/trainers/add_pokeball", headers = HEADR, json = body_catch_in_a_pokeball)
-print(catch_in_a_pokeball.text)"""
+catch_in_a_pokeball = requests.post(url = f"{URL}/trainers/add_pokeball", headers = HEADR, json = body_catch_in_a_pokeball)
+print(catch_in_a_pokeball.text)
